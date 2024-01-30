9ca1b85244fe1145908614f958ee82ee7197b5b8
andradajus <138541478+andradajus@users.noreply.github.com>
2024-01-31 00:27:13 +0800

    Tweak tailwind configuration, initial login UI setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,26 +4,11 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   daisyui: {
-    styled: true,
+    styled: true, 
     utils: true,
-    themes: ["autumn", "luxury"]
-    //   {
-    //     mytheme: {
-    //     "primary": "#00b7ff",
-    //     "secondary": "#00e700",
-    //     "accent": "#008cff",
-    //     "neutral": "#0a0f03",
-    //     "base-100": "#281e25",
-    //     "info": "#00ffff",
-    //     "success": "#82f600",
-    //     "warning": "#e67200",
-    //     "error": "#ff0048",
-    //     },
-    //   },
-    // ],
+    themes: ["autumn", "luxury"],
   },
     extend: {},
     // eslint-disable-next-line no-undef
-   plugins: [require("@tailwindcss/typography"), require("daisyui")],
+    plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }
-
