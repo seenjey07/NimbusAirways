788ff165be6ff9067bf791c6e334a685dd66d7c9
andradajus <138541478+andradajus@users.noreply.github.com>
2024-01-31 21:21:26 +0800

    Created initial dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/home";
+import Home from "./pages/home/Home";
 import Login from "./pages/Login";
+import DashboardLayout from "./layouts/dashboardlayout";
 import Registration from "./pages/Registration";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/dashboard/*" element={<DashboardLayout />} />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<Registration />} />
           <Route path="/" element={<Home />} />
