cfb331052e59ef270b30059e0a5f2abdd4c171ba
andradajus <138541478+andradajus@users.noreply.github.com>
2024-01-31 18:40:25 +0800

    Modified registration and login components

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -27,9 +27,9 @@ const Login = () => {
   return (
     <>
       <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero-content flex-col lg:flex-row-reverse gap-16">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold gap-12">Nimbus Airways</h1>
+            <h1 className="text-5xl font-bold">Nimbus Airways</h1>
             <p className="py-6 italic">
               Discover the Skies with Nimbus Airways: Elevating Your Journey
               Beyond Boundaries
@@ -37,37 +37,37 @@ const Login = () => {
           </div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="Email"
-                  className="input input-bordered"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  placeholder="Password"
-                  className="input input-bordered"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-                <label className="label">
-                  <button className="label-text-alt link link-hover">
-                    Forgot password?
-                  </button>
-                </label>
-              </div>
+                <div className="form-control">
+                    <label className="label">
+                    <span className="label-text">Email</span>
+                    </label>
+                    <input
+                    type="email"
+                    placeholder="Email"
+                    className="input input-bordered"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                    <span className="label-text">Password</span>
+                    </label>
+                    <input
+                    type="password"
+                    placeholder="Password"
+                    className="input input-bordered"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    />
+                    <label className="label">
+                    <button className="label-text-alt link link-hover">
+                        Forgot password?
+                    </button>
+                    </label>
+                </div>
               <div className="form-control mt-6">
                 <button className="btn btn-primary" onClick={handleLogin}>
                   Login
@@ -85,7 +85,7 @@ const Login = () => {
                 <span>Login with Google</span>
               </button>
             </div>
-            <div className="flex justify-center">
+            <div className="flex justify-center mb-3">
               <label className="label">
                 <span className="label-text-alt mr-1">
                   Dont have an account?
--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,4 +1,5 @@
 const Registration = () => {
+  
   return (
     <>
       <div className="card lg:card-side bg-base-100 shadow-xl">
@@ -52,7 +53,7 @@ const Registration = () => {
               <input
                 type="date"
                 placeholder="Type here"
-                className="input input-bordered w-full max-w-xs"
+                className="input input-bordered w-full max-w-xs text-center"
                 required
               />
             </label>
@@ -70,8 +71,70 @@ const Registration = () => {
             </label>
           </div>
 
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Listen</button>
+          <div className="flex justify-center gap-16">
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Email</span>
+              </div>
+              <input
+                type="email"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+                required
+              />
+            </label>
+
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Gender</span>
+              </div>
+              <select className="select select-bordered" required>
+                <option disabled selected>Pick one</option>
+                <option>Male</option>
+                <option>Female</option>
+              </select>
+            </label>
+          </div>
+
+          <div className="flex justify-center gap-16">
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="password"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+                required
+              />
+            </label>
+
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Confirm Password</span>
+              </div>
+              <input
+                type="password"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+                required
+              />
+            </label>
+          </div>
+
+          <div className="flex justify-center">
+            <label className="label">
+            <input type="checkbox" className="checkbox checkbox-xs mr-2" />
+              <div className="label-text">I agree with the 
+              <span className="ml-1 label-text-alt link link-hover">Terms and Conditions</span>
+              </div> 
+            </label>
+          </div>
+
+          
+
+          <div className="card-actions justify-center mt-12">
+            <button className="btn btn-primary">Submit</button>
           </div>
         </div>
       </div>
