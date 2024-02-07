d3d26021f4807dd5c6bf2698e7be93258343bc0c
andradajus <138541478+andradajus@users.noreply.github.com>
2024-02-08 00:38:08 +0800

    Created dashboard layout and adjusted footer and navbar

--- frontend/src/pages/FlightsSearch.jsx
+++ frontend/src/pages/FlightsSearch.jsx
@@ -1,14 +1,13 @@
 import "../App.css";
 import { useState, useEffect } from "react";
-import Navbar from "../components/UserNavBar";
 import {
   flightsApi,
   indexedFlightsApi,
   indexedRoutesApi,
 } from "../lib/flightsapi";
-import Footer from "./home/footer";
 import FlightSearchOrigin from "../components/flightsearchorigin";
 import format from "date-fns/format";
+import UserDashboardLayout from "../layouts/UserDashboardLayout";
 
 const FlightsSearchComponent = () => {
   const [origin_location, setOrigin_location] = useState("");
@@ -68,152 +67,147 @@ const FlightsSearchComponent = () => {
 
   return (
     <>
-      <div>
-        <Navbar />
-      </div>
+      <UserDashboardLayout>
+        <div className="flex justify-center gap-5 shadow-xl my-4">
+          <div className="flex justify-center gap-5">
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Origin Location:</span>
+              </div>
+              <FlightSearchOrigin
+                originOptions={originOptions}
+                onSelect={handleOriginSelect}
+              />
+            </label>
 
-      <div className="flex justify-center gap-5 shadow-xl my-4">
-        <div className="flex justify-center gap-5">
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">Origin Location:</span>
-            </div>
-            <FlightSearchOrigin
-              originOptions={originOptions}
-              onSelect={handleOriginSelect}
-            />
-          </label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Destination:</span>
+              </div>
+              <input
+                className="input input-bordered w-full max-w-xs"
+                type="text"
+                placeholder="Type here"
+                value={destination_location}
+                onChange={(e) => setDestination_location(e.target.value)}
+              />
+            </label>
 
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">Destination:</span>
-            </div>
-            <input
-              className="input input-bordered w-full max-w-xs"
-              type="text"
-              placeholder="Type here"
-              value={destination_location}
-              onChange={(e) => setDestination_location(e.target.value)}
-            />
-          </label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Date:</span>
+              </div>
+              <input
+                className="input input-bordered w-full max-w-xs"
+                type="date"
+                value={departure_date}
+                onChange={(e) => setDeparture_date(e.target.value)}
+              />
+            </label>
 
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">Date:</span>
-            </div>
-            <input
-              className="input input-bordered w-full max-w-xs"
-              type="date"
-              value={departure_date}
-              onChange={(e) => setDeparture_date(e.target.value)}
-            />
-          </label>
-
-          <div className="card-actions justify-center my-6">
-            <button
-              onClick={handleSearch}
-              type="submit"
-              className="btn btn-primary"
-            >
-              Search
-            </button>
-          </div>
-
-          {error && (
-            <div role="alert" className="alert alert-error">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="strokeCurrent shrink-0 h-2 w-2"
-                fill="none"
-                viewBox="0 0 24 24"
+            <div className="card-actions justify-center my-6">
+              <button
+                onClick={handleSearch}
+                type="submit"
+                className="btn btn-primary"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span className="text-white">{error}</span>
+                Search
+              </button>
             </div>
-          )}
-        </div>
-      </div>
 
-      {isInitialLoad && initialLoadFlights.length >= 0 && (
-        <div className="overflow-x-auto mt-4">
-          <table className="table table-zebra table-pin-cols text-center">
-            <thead>
-              <tr className="text-black">
-                <th>Flight Number</th>
-                <th>Origin Location</th>
-                <th>Departure Date</th>
-                <th>Departure Location</th>
-                <th>Arrival Date</th>
-                <th>Price</th>
-              </tr>
-            </thead>
-            <tbody className="text-center">
-              {initialLoadFlights.map((flight) => (
-                <tr className="hover" key={flight.flight_number}>
-                  <td>{flight.flights.flight_number}</td>
-                  <td>{flight.routes.origin_location}</td>
-                  <td>
-                    {format(
-                      new Date(flight.flights.departure_date),
-                      "MMMM dd, yyyy hh:mm a"
-                    )}
-                  </td>
-                  <td>{flight.routes.destination_location}</td>
-                  <td>
-                    {format(
-                      new Date(flight.flights.arrival_date),
-                      "MMMM dd, yyyy hh:mm a"
-                    )}
-                  </td>
-                  <td>₱ {flight.routes.price}</td>
+            {error && (
+              <div role="alert" className="alert alert-error">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="strokeCurrent shrink-0 h-2 w-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="1.5"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span className="text-white">{error}</span>
+              </div>
+            )}
+          </div>
+        </div>
+
+        {isInitialLoad && initialLoadFlights.length >= 0 && (
+          <div className="overflow-x-auto mt-4">
+            <table className="table table-zebra table-pin-cols text-center">
+              <thead>
+                <tr className="text-black">
+                  <th>Flight Number</th>
+                  <th>Origin Location</th>
+                  <th>Departure Date</th>
+                  <th>Departure Location</th>
+                  <th>Arrival Date</th>
+                  <th>Price</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
-
-      {!isInitialLoad && flights.length >= 1 && (
-        <div className="overflow-x-auto mt-4">
-          <table className="table table-zebra table-pin-cols">
-            <thead>
-              <tr>
-                <th>Flight Number</th>
-                <th>Origin Location</th>
-                <th>Destination Location</th>
-                <th>Departure Date</th>
-                <th>Price</th>
-              </tr>
-            </thead>
-            <tbody>
-              {flights
-                .sort((a, b) =>
-                  a.departure_date && b.departure_date
-                    ? a.departure_date.localeCompare(b.departure_date)
-                    : 0
-                )
-                .map((flight) => (
+              </thead>
+              <tbody className="text-center">
+                {initialLoadFlights.map((flight) => (
                   <tr className="hover" key={flight.flight_number}>
-                    <td>{flight.flight_number}</td>
-                    <td>{flight.origin_location}</td>
-                    <td>{flight.destination_location}</td>
-                    <td>{flight.departure_date}</td>
-                    <td>₱ {flight.price}</td>
+                    <td>{flight.flights.flight_number}</td>
+                    <td>{flight.routes.origin_location}</td>
+                    <td>
+                      {format(
+                        new Date(flight.flights.departure_date),
+                        "MMMM dd, yyyy hh:mm a"
+                      )}
+                    </td>
+                    <td>{flight.routes.destination_location}</td>
+                    <td>
+                      {format(
+                        new Date(flight.flights.arrival_date),
+                        "MMMM dd, yyyy hh:mm a"
+                      )}
+                    </td>
+                    <td>₱ {flight.routes.price}</td>
                   </tr>
                 ))}
-            </tbody>
-          </table>
-        </div>
-      )}
-      <div>
-        <Footer />
-      </div>
+              </tbody>
+            </table>
+          </div>
+        )}
+
+        {!isInitialLoad && flights.length >= 1 && (
+          <div className="overflow-x-auto mt-4">
+            <table className="table table-zebra table-pin-cols">
+              <thead>
+                <tr>
+                  <th>Flight Number</th>
+                  <th>Origin Location</th>
+                  <th>Destination Location</th>
+                  <th>Departure Date</th>
+                  <th>Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {flights
+                  .sort((a, b) =>
+                    a.departure_date && b.departure_date
+                      ? a.departure_date.localeCompare(b.departure_date)
+                      : 0
+                  )
+                  .map((flight) => (
+                    <tr className="hover" key={flight.flight_number}>
+                      <td>{flight.flight_number}</td>
+                      <td>{flight.origin_location}</td>
+                      <td>{flight.destination_location}</td>
+                      <td>{flight.departure_date}</td>
+                      <td>₱ {flight.price}</td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </UserDashboardLayout>
     </>
   );
 };
