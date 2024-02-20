f17cd6579027ad36c6e59a45fa8904490da7452f
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-02-20 16:06:00 +0800

    Modified email contents for users.

---
+++ backend/test/controllers/bookings_controller_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class BookingsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @user = users(:one) 
+    sign_in @user
+  end
+
+  test 'should get index' do
+    get bookings_url
+    assert_response :success
+  end
+
+  test 'should get no bookings notice when user has no bookings' do
+    get bookings_url
+    assert_includes @response.body, 'You have no bookings.'
+  end
+
+  test 'should get formatted bookings when user has bookings' do
+    booking = bookings(:one) 
+    get bookings_url
+    assert_includes @response.body, booking.booking_reference
+  end
+
+  test 'should create booking' do
+    assert_difference('Booking.count') do
+      post bookings_create_booking_url, params: { booking: { flight_id: flights(:one).id } }
+    end
+
+    assert_response :created
+  end
+
+  test 'should return errors when unable to create booking' do
+    post bookings_create_booking_url, params: { booking: { invalid_params_here } }
+    assert_response :unprocessable_entity
+  end
+
+end
